aeee755c2efb9fbd607187e9559a54b0ec9914ed
YONG WERN JIE <129282000+Wernjie0713@users.noreply.github.com>
2026-01-06 10:36:43 +0800

    npm run build

--- public/build/assets/ApplicationLogo-DZ3cp5zd.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-qetWhnOG.js";function r(o){return t.jsx("img",{...o,src:"/storage/images/logo.png",alt:"UTMDuitNow Logo"})}export{r as A};
---
+++ public/build/assets/ApplicationLogo-DgQIk4Ie.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-BJdaTNqI.js";function r(o){return t.jsx("img",{...o,src:"/storage/images/logo.png",alt:"UTMDuitNow Logo"})}export{r as A};
